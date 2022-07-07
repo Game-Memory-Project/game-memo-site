e583f1df90ce1fb5c4b0126a9f28e005b22cd64d
Laís Galvão <laisgbueno62@gmail.com>
2022-07-07 10:42:29 -0300

    feat: menu mobile e footer

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,16 @@
 <script setup>
-import Header from './components/Header.vue';
-import Footer from './components/Footer.vue';
+import Header from "./components/Header.vue";
+import Footer from "./components/Footer.vue";
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-
 </script>
 
 <template>
-  <Header></Header>
+  <div class="flex flex-col h-screen">
+    <Header></Header>
     <router-view></router-view>
-  <Footer></Footer>
+    <Footer></Footer>
+  </div>
 </template>
 
 <style>
--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,13 +1,25 @@
 <template>
-  meu footer
+  <footer
+    class="
+      flex
+      items-center
+      justify-between
+      flex-wrap
+      bg-teal-500
+      p-6
+      sticky
+      top-[100vh]
+    "
+  >
+    <div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto text-white mr-6">
+      <span>Meu footer</span>
+    </div>
+  </footer>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
 <style>
-
 </style>
